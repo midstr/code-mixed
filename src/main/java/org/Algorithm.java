75858ab90f5e3e5b54cea36723a57c7dafeda778
liyaogang <liyaogang@meituan.com>
2016-10-26 15:34:51 +0800

    quick sort

--- src/main/java/org/Algorithm.java
+++ src/main/java/org/Algorithm.java
@@ -18,6 +18,9 @@ public class Algorithm {
         int max = testOOM.maxSubArray(ary);
         System.out.println(max);
 
+        testOOM.quickSort(ary);
+        System.out.println(Arrays.toString(ary));
+
         int[] sorted = testOOM.sort(ary);
         System.out.println(Arrays.toString(sorted));
 
@@ -41,6 +44,32 @@ public class Algorithm {
             }
         }
         return max;
+    }
+
+    public void quickSort(int[] arr) {
+        qsort(arr, 0, arr.length - 1);
+    }
+
+    private void qsort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);        //将数组分为两部分
+            qsort(arr, low, pivot - 1);                   //递归排序左子数组
+            qsort(arr, pivot + 1, high);                  //递归排序右子数组
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];     //枢轴记录
+        while (low < high) {
+            while (low < high && arr[high] >= pivot) --high;
+            arr[low] = arr[high];             //交换比枢轴小的记录到左端
+            while (low < high && arr[low] <= pivot) ++low;
+            arr[high] = arr[low];           //交换比枢轴小的记录到右端
+        }
+        //扫描完成，枢轴到位
+        arr[low] = pivot;
+        //返回的是枢轴的位置
+        return low;
     }
 
     public int[] sort(int[] nums) {
